13c13c8e936bb0627836115d9fee0a9b61602824
Guilherme Aranega <guilhermee.aranega@gmail.com>
2026-06-10 21:31:15 -0300

    FIX: styles from worker not working

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,14 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
 import { cloudflare } from "@cloudflare/vite-plugin";
 import tsConfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
     cloudflare({ viteEnvironment: { name: "ssr" } }),
     tanstackStart(),
     react(),
+    tailwindcss(),
     tsConfigPaths(),
   ],
 
